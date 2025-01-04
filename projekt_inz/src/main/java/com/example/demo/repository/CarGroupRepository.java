67313974871a69225658554bfc8a78382568cf42
Jagodziński Cezary <cza-jag@wp.pl>
2025-01-04 17:52:17 +0100

    zero

--- projekt_inz/src/main/java/com/example/demo/repository/CarGroupRepository.java
+++
@@ -1,8 +0,0 @@
-// CarGroupRepository.java
-package com.example.demo.repository;
-
-import com.example.demo.entity.CarGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarGroupRepository extends JpaRepository<CarGroup, Long> {
-}
